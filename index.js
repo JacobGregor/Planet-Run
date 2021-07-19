af471aed55c7cb7b4a4e91aa9f8fd9b8aac93578
Jacob Gregor <67080552+JacobGregor@users.noreply.github.com>
2021-07-18 17:27:09 -0700

    character-movement. Object clean-up.

--- index.js
+++ index.js
@@ -1,124 +1,96 @@
 //might be able to style the canvas width in css and not have to declare that in here... Curren tly declared in home.css
 // canvas.width = 1220;
 // canvas.height = 500;
-
 const canvas = document.querySelector('canvas')
-const context = canvas.getContext('2d')
 
 canvas.width = innerWidth
-canvas.height =500;
+canvas.height = 500;
 
-// Creates Player Character.
-class Player {
-    constructor(height,width,x,y,color,jumping,xVelocity,yVelocity) {
+const context = canvas.getContext('2d')
+const ArrowKey = {
+    RIGHT: 'ArrowRight',
+    LEFT: 'ArrowLeft',
+    UP: 'ArrowUp'
+};
+    
+class CanvasBase {
+    constructor (height,width,xCoord,yCoord) {
         this.height = height;
         this.width = width;
-        this.x = x;
-        this.y = y;
-        this.color = color;
-        this.jumping = true;
-        this.xVelocity = xVelocity;
-        this.yVelocity = yVelocity
-
-    }
-    draw(){
-        context.beginPath()
-        context.rect(this.x,this.y,this.width,this.height)
-        context.fillStyle = this.color;
-        context.fill()
+        this.xCoord = xCoord;
+        this.yCoord = yCoord;
         
     }
-
-}
-
-// Player Movement
-
-const plrControls = {
-//sets the initial value of left,right & up to false.
-    left: false,
-    right: false,
-    up: false,
-//key event function is listening for left,right or up to generate
-    keyEvent: function (event) {
-
-        let keyPress = (KeyboardEvent == "keydown") ? true : false;
-//keys determines which key is being pressed
-        switch(event.keys){
-            case 37: plrControls.left = keyPress
-            break;
-            case 39: plrControls.right = keyPress
-            break;
-            case 38: plrControls.up = keyPress
-            break;
-        };
-
-
+    draw (context) {
+        console.log('context',context)
+        context.beginPath()
+        context.rect(this.xCoord,this.yCoord,this.width,this.height)
+        context.fillStyle = 'green';
+        context.
+        context.closePath()
+        context.fill()
     }
 };
 
-
-const loop = function () {
-    if (plrControls.up && Player.jumping == false) {
-        Player.yVelocity -= 20;
-        Player.jumping = true;
+class Character extends CanvasBase {
+    constructor(health,x,y) {
+        super(40,20,x,y)
+        this.health = health;
     }
 
-    if (plrControls.left) {
-        Player.xVelocity -= 0.5;
+    moveRight() {
+        this.xCoord = this.xCoord + 5;
+        this.draw(context)
     }
-    
-    if (plrControls.right) {
-        Player.xVelocity += 0.5
+    moveLeft() {
+        this.xCoord = this.xCoord - 5;
+        this.draw(context)
     }
-
+    moveUp() {
+        this.yCoord = this.yCoord - 5;
+        this.draw(context)
+    }
 }
 
-
-
-class projectile {
+class Projectile extends CanvasBase {
     constructor(x,y,radius,color,velocity){
-        this.x = x; 
-        this.y = y;  
+        super(undefined,undefined,x,y)
         this.radius = radius; 
         this.color = color; 
         this.velocity = velocity; 
     }
     draw(){
         context.beginPath()
-        context.arc(this.x, this.y, this.radius, 0, Math.PI * 2,false)
+        context.arc(this.xCoord, this.yCoord, this.radius, 0, Math.PI * 2,false)
         context.fillStyle = this.color;
         context.fill()
     }
 }
 
-//Create Enemy(Alien) Unit
-// class Enemy {
-//     constructor(height,width,x,y,color,velocity) {
-//         this.height = height;
-//         this.width = width;
-//         this.x = x;
-//         this.y = y;
-//         this.color = color;
-//         this.velocity = velocity;
 
-//     }
-//     draw(){
-//         context.beginPath()
-//         context.rect(this.x,this.y,this.width,this.height)
-//         context.fillStyle = this.color;
-//         context.fill()
-        
-//     }
+const character = new Character(100, 25, 460)
+character.draw(context);
 
-// }
-
-const player = new Player(40,20,25,460,'green',true,0,0)
-player.draw();
-
-
-const fire = addEventListener('click',(event) => 
-{
+const fire = addEventListener('click',(event) => {
     console.log(event)
-    const Projectile = new projectile(event.clientX,event.clientY,2,'red',null)
-    Projectile.draw();
+    const projectile = new Projectile(event.clientX,event.clientY,2,'red',null)
+    projectile.draw();
 });
+
+const plrMovement = (event) => {
+    switch(event.key){
+        case ArrowKey.LEFT: 
+            character.moveLeft()
+            break;
+        case ArrowKey.RIGHT: 
+            character.moveRight()
+            break;
+        case ArrowKey.UP: 
+            character.moveUp()
+            break;
+        default:
+            break;
+    };
+}
+
+window.addEventListener('keydown', plrMovement)
